25ea4106214aa40dc65746c91cf82de856f80549
Tomdooo_ <tomdo_@fpproduction.eu>
2022-11-19 14:51:28 +0100

    initial commit

---
+++ src/main/java/cz/uhk/tj_strat_rk/TjStratRkApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.tj_strat_rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjStratRkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjStratRkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/tj_strat_rk/TjStratRkApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.tj_strat_rk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjStratRkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
