a349db0522b34370f5b218218ab935d26045f758
Tomdooo_ <tomdo_@fpproduction.eu>
2022-11-20 11:42:23 +0100

    Updated Member, first database version done

--- src/main/java/cz/uhk/tj_strat_rk/model/Member.java
+++ src/main/java/cz/uhk/tj_strat_rk/model/Member.java
@@ -1,9 +1,6 @@
 package cz.uhk.tj_strat_rk.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,16 +13,9 @@ public class Member {
     private String role;
     private String firstName;
     private String lastName;
-    //TODO Add Team
-    //CONSTRUCTOR
-    public Member(String role, String firstName, String lastName) {
-        this.role = role;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 
-    public Member() {
-    }
+    @ManyToOne
+    private Team team;
 
     @OneToMany(mappedBy = "member")
     private List<Training> trainings = new ArrayList<>();
@@ -33,6 +23,24 @@ public class Member {
     @OneToMany(mappedBy = "ministration")
     private List<Event> events = new ArrayList<>();
 
+    //CONSTRUCTOR
+    public Member(String role, String firstName, String lastName) {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Member(String role, String firstName, String lastName, Team team) {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team = team;
+    }
+
+    public Member() {
+    }
+
+    // GETTERS AND SETTERS
     public int getId() {
         return id;
     }
@@ -71,5 +79,21 @@ public class Member {
 
     public void setTrainings(List<Training> trainings) {
         this.trainings = trainings;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
