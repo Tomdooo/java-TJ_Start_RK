4a39b9f0c7097fa5c8d8c5f33ed510bda6bb360b
Václav Buřil <vasekburil@seznam.cz>
2022-11-20 10:58:56 +0100

    Finish Member.java

--- src/main/java/cz/uhk/tj_strat_rk/model/Member.java
+++ src/main/java/cz/uhk/tj_strat_rk/model/Member.java
@@ -1,5 +1,72 @@
 package cz.uhk.tj_strat_rk.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Members")
 public class Member {
+    @Id
+    @GeneratedValue
+    private int id;
+
     private String role;
+    private String firstName;
+    private String lastName;
+    //TODO Add Team
+    //CONSTRUCTOR
+    public Member(String role, String firstName, String lastName) {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Member() {
+    }
+
+    @OneToMany(mappedBy = "member")
+    private List<Training> trainings = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Training> getTrainings() {
+        return trainings;
+    }
+
+    public void setTrainings(List<Training> trainings) {
+        this.trainings = trainings;
+    }
 }
