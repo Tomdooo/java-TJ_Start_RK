fe487617512cd0d07d10329bfd82fb14007906ba
Václav Buřil <vasekburil@seznam.cz>
2023-01-05 14:13:32 +0100

    add not funtions docker

--- src/main/java/cz/uhk/tj_start_rk/TjStartRkApplication.java
+++ src/main/java/cz/uhk/tj_start_rk/TjStartRkApplication.java
@@ -1,16 +1,16 @@
-//package cz.uhk.tj_start_rk;
-//
-//import cz.uhk.tj_start_rk.config.RsaKeyProperties;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//
-//@EnableConfigurationProperties(RsaKeyProperties.class)
-//@SpringBootApplication
-//public class TjStartRkApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(TjStartRkApplication.class, args);
-//    }
-//
-//}
+package cz.uhk.tj_start_rk;
+
+import cz.uhk.tj_start_rk.config.RsaKeyProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@SpringBootApplication
+public class TjStartRkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjStartRkApplication.class, args);
+    }
+
+}
